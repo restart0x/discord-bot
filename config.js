d48019c1b1c9abf473f6bf1a2c0366431aa42b8c
restart0x <161155505+restart0x@users.noreply.github.com>
2024-05-01 20:50:56 -0400

    improve error handling

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+
+// Check for the presence of BOT_TOKEN in the environment variables
+if (!process.env.BOT_TOKEN) {
+  console.error("ERROR: The BOT_TOKEN environment variable is not set.");
+  process.exit(1); // Exit the process with an error code
+}
+
 module.exports = {
   botToken: process.env.BOT_TOKEN,
-  prefix: process.env.PREFIX || "!",
+  prefix: process.env.PREFIX || "!", // Use "!" as the default prefix if none is provided
 };
