8c751086dbbe71b7ded1a828b9aa740cd37b4d42
restart0x <161155505+restart0x@users.noreply.github.com>
2024-05-03 06:51:35 -0400

    enhance readability

--- utils.js
+++ utils.js
@@ -1,32 +1,26 @@
 require('dotenv').config();
 
-function log(message) {
-    console.log(`[${new Date().toISOString()}] ${message}`);
-}
+const log = (message) => console.log(`[${new Date().toISOString()}] ${message}`);
 
-function logError(error, message = 'An error occurred') {
-    console.error(`[${new Date().toISOString()}] ${message}:`, error);
-}
+const logError = (error, message = 'An error occurred') => console.error(`[${new Date().toISOString()}] ${message}:`, error);
 
-function safeJSONParse(str) {
+const safeJSONParse = (str) => {
     try {
         return [null, JSON.parse(str)];
     } catch (error) {
         return [error];
     }
-}
+};
 
-function checkEnvVariable(varName) {
+const checkEnvVariable = (varName) => {
     if (!process.env[varName]) {
         logError(`Environment variable ${varName} is not set`);
         return false;
     }
     return true;
-}
+};
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = {
     log,
