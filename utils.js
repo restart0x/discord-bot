57e64d43b2a3d21ddebdb347e05f0f81a681fb58
restart0x <161155505+restart0x@users.noreply.github.com>
2024-05-04 13:49:01 -0400

    error handling update

--- utils.js
+++ utils.js
@@ -2,19 +2,22 @@ require('dotenv').config();
 
 const log = (message) => console.log(`[${new Date().toISOString()}] ${message}`);
 
-const logError = (error, message = 'An error occurred') => console.error(`[${new Date().toISOString()}] ${message}:`, error);
+const logError = (error, message = 'An error occurred', details = '') => {
+    const detailMessage = details ? ` Details: ${details}` : '';
+    console.error(`[${new Date().toISOString()}] ${message}:${detailMessage}`, error);
+};
 
 const safeJSONParse = (str) => {
     try {
         return [null, JSON.parse(str)];
     } catch (error) {
-        return [error];
+        return [new Error(`Failed to parse JSON string. Original error: ${error.message}`)];
     }
 };
 
 const checkEnvVariable = (varName) => {
     if (!process.env[varName]) {
-        logError(`Environment variable ${varName} is not set`);
+        logError(new Error(`MissingEnvVar`), `Environment variable '${varName}' is not set. Ensure it is declared in your environment.`);
         return false;
     }
     return true;
@@ -22,10 +25,19 @@ const checkEnvVariable = (varName) => {
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+async function withErrorLogging(fn) {
+    try {
+        await fn();
+    } catch (error) {
+        logError(error, `Error executing async function '${fn.name}'.`);
+    }
+}
+
 module.exports = {
     log,
     logError,
     safeJSONParse,
     checkEnvVariable,
-    sleep
+    sleep,
+    withErrorLogging
 };
