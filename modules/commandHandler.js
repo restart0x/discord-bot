efcc8455941048a348f178ba4b385547d6254a56
restart0x <161155505+restart0x@users.noreply.github.com>
2024-05-04 02:11:54 -0400

    optimize the naming

--- modules/commandHandler.js
+++ modules/commandHandler.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
-const commandFunctions = {};
+
+const commandExecutors = {};
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commandFunctions[command.name] = command.execute;
+    commandExecutors[command.name] = command.execute;
 }
-const processMessage = async (message) => {
+
+const handleMessage = async (message) => {
     if (!message.content.startsWith('/') || message.author.bot) return;
     const args = message.content.slice(1).split(/ +/);
     const commandName = args.shift().toLowerCase();
-    if (!commandFunctions[commandName]) {
+
+    if (!commandExecutors[commandName]) {
         message.reply(`Sorry, I don't recognize that command.`);
         return;
     }
     try {
-        await commandFunctions[commandName](message, args);
+        await commandExecutors[commandName](message, args);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute that command!');
     }
 };
-module.exports = { processMessage };
+
+module.exports = { processMessage: handleMessage };
