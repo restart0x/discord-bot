fe23e22f47edc011bf21ef4fd641ddd0af32130e
restart0x <161155505+restart0x@users.noreply.github.com>
2024-05-08 06:33:11 -0400

    optimize the naming

--- modules/commandHandler.js
+++ modules/commandHandler.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const commandHandlers = {};
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandHandlers = new Map();
+const path = require('path');
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const commandModule = require(`./commands/${file}`);
-    commandHandlers[commandModule.name] = commandModule.execute;
+    const commandModule = require(path.join(commandsPath, file));
+    commandHandlers.set(commandModule.name, commandModule.execute);
 }
 
 const handleDiscordMessage = async (message) => {
@@ -16,13 +18,13 @@ const handleDiscordMessage = async (message) => {
     const args = message.content.slice(1).split(/ +/);
     const commandName = args.shift().toLowerCase();
 
-    if (!commandHandlers[commandName]) {
+    if (!commandHandlers.has(commandName)) {
         message.reply(`Sorry, I don't recognize that command.`);
         return;
     }
     
     try {
-        await commandHandlers[commandName](message, args);
+        await commandHandlers.get(commandName)(message, args);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute that command!');
