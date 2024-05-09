e994dc069bd5ef55ea3f4475f5c4ca7da5a09f54
restart0x <161155505+restart0x@users.noreply.github.com>
2024-05-09 13:45:49 -0400

    improve code readability

--- modules/commandHandler.js
+++ modules/commandHandler.js
@@ -2,29 +2,31 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const commandHandlers = new Map();
 const path = require('path');
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandHandlers = new Map();
 
-for (const file of commandFiles) {
-    const commandModule = require(path.join(commandsPath, file));
-    commandHandlers.set(commandModule.name, commandModule.execute);
-}
+fs.readdirSync(commandsPath)
+  .filter(file => file.endsWith('.js'))
+  .forEach(file => {
+      const commandModule = require(path.join(commandsPath, file));
+      commandHandlers.set(commandModule.name, commandModule.execute);
+  });
 
 const handleDiscordMessage = async (message) => {
     if (!message.content.startsWith('/') || message.author.bot) return;
-    
-    const args = message.content.slice(1).split(/ +/);
-    const commandName = args.shift().toLowerCase();
 
-    if (!commandHandlers.has(commandName)) {
+    const args = message.content.slice(1).split(/ +/);
+    const [commandName, ...restArgs] = args;
+    const command = commandHandlers.get(commandName.toLowerCase());
+
+    if (!command) {
         message.reply(`Sorry, I don't recognize that command.`);
         return;
     }
     
     try {
-        await commandHandlers.get(commandName)(message, args);
+        await command(message, restArgs);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute that command!');
